68b6815d4957aaa4d477430483573cf21d8e6616
WeepingDogel <weepingdogel@gmail.com>
2025-06-08 01:55:49 +0800

    Updated LFS user permission settings, fixed Ncurses source code compilation script, and added missing Bash patches.

--- src/step4/compile-bash.sh
+++ src/step4/compile-bash.sh
@@ -5,6 +5,14 @@ export LFS=/mnt/lfs
 export LC_ALL=POSIX
 export LFS_TGT=$(uname -m)-gozjaro-linux-gnu
 
+cp -v ../bash-5.2_p15-random-ub.patch "$LFS/sources"
+cp -v ../bash-5.2_p21-wpointer-to-int.patch "$LFS/sources"
+cp -v ../bash-5.2_p32-memory-leaks.patch "$LFS/sources"
+cp -v ../bash-5.2_p32-invalid-continuation-byte-ignored-as-delimiter-1.patch "$LFS/sources"
+cp -v ../bash-5.2_p32-invalid-continuation-byte-ignored-as-delimiter-2.patch "$LFS/sources"
+cp -v ../bash-5.2_p32-erroneous-delimiter-pushback-condition.patch "$LFS/sources"
+
+
 cd "$LFS/sources" || exit 1
 
 # Extract Bash source package
@@ -27,11 +35,22 @@ patch -Np0 -i ../bash-5.2_p32-invalid-continuation-byte-ignored-as-delimiter-1.p
 patch -Np0 -i ../bash-5.2_p32-invalid-continuation-byte-ignored-as-delimiter-2.patch
 patch -Np0 -i ../bash-5.2_p32-erroneous-delimiter-pushback-condition.patch
 
+_bashconfig=(-DDEFAULT_PATH_VALUE=\'\"/usr/local/sbin:/usr/local/bin:/usr/bin\"\'
+               -DSTANDARD_UTILS_PATH=\'\"/usr/bin\"\'
+               -DSYS_BASHRC=\'\"/etc/bash.bashrc\"\'
+               -DSYS_BASH_LOGOUT=\'\"/etc/bash.bash_logout\"\'
+               -DNON_INTERACTIVE_LOGIN_SHELLS
+               -std=gnu17)
+
+ export CFLAGS="${CFLAGS} ${_bashconfig[@]}"
 
 # Configure Bash
 ./configure \
     --prefix=/usr \
     --build=$(sh support/config.guess) \
+    --with-curses \
+    --enable-readline \
+    --with-installed-readline \
     --host="$LFS_TGT" \
     --without-bash-malloc \
     bash_cv_strtold_broken=no || exit 1
